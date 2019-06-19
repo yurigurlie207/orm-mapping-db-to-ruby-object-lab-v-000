127e26284b775a755e768cd36d7de7a231c68e8f
Adrienne Yao <adrienne.yao@icloud.com>
2019-06-19 22:21:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -84,9 +84,9 @@ class Student
       WHERE grade < 12
     SQL
 
-        DB[:conn].execute(sql).map do |row|
-          self.new_from_db(row)
-        end
-      end
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
 
 end
