88dc82f5c105cc541db2c5a4f1752782a0357a0c
Adrienne Yao <adrienne.yao@icloud.com>
2019-06-19 22:27:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -127,7 +127,7 @@ class Student
 
       DB[:conn].execute(sql).map do |row|
         self.new_from_db(row)
-      end.first #this is needed because there is just one row
+      end
     end
 
 end
