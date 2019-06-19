32e2a28e4af6f9a06559ca1128284d50173f0a7d
Adrienne Yao <adrienne.yao@icloud.com>
2019-06-19 22:25:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -103,4 +103,18 @@ class Student
     end
   end
 
+  def self.first_student_in_grade_10(xnum)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      ORDER BY students.id
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql,xnum).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
 end
