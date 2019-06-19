54711af5df674357b3250cd16e342a8c76941999
Adrienne Yao <adrienne.yao@icloud.com>
2019-06-19 22:10:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,6 +13,7 @@ class Student
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    
   end
 
   def self.find_by_name(name)
